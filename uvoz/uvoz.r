b0894301e01bb395f048f652f5d9fc9da7cb78ff
Anja Zavrl <zavrl.anja@gmail.com>
2014-12-05 18:57:58 +0100

    Uvoz tabel

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,16 +1,31 @@
 # 2. faza: Uvoz podatkov
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
+# Funkcija, ki uvozi podatke iz datoteke sezona2014.csv
+uvoziSezona2014 <- function() {
+  return(read.table("podatki/sezona2014.csv", sep = ";", as.is = TRUE,
+                      row.names = 1, header = TRUE,
                       fileEncoding = "Windows-1250"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
+# Zapišimo podatke v razpredelnico sezona 2014.
+cat("Uvažam podatke o sezoni 2014...\n")
+sezona2014 <- uvoziSezona2014()
+
+
+
+
+# Funkcija, ki uvozi podatke iz datoteke svetovniprvaki.csv
+uvoziSvetovniprvaki <- function() {
+  return(read.table("podatki/svetovniprvaki.csv", sep = ";", as.is = TRUE,
+                    header = TRUE,
+                    fileEncoding = "Windows-1250"))
+}
+
+
+# Zapišimo podatke v razpredelnico svetovni prvaki.
+cat("Uvažam podatke o svetovnih prvakih...\n")
+svetovniprvaki <- uvoziSvetovniprvaki()
+
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
