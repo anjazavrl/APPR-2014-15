a38eb66bc9374b32522367676894a1503857f411
Anja Zavrl <zavrl.anja@gmail.com>
2014-12-06 13:14:56 +0100

    popravek na grafu

--- slike/grafi.R
+++ slike/grafi.R
@@ -1,5 +1,5 @@
 pdf("slike/graf.pdf", paper="a4")
 barplot(sezona2014$Points[1:5], main = "Število točk prvih pet dirkačev v sezoni 2014", 
-        xlab="Dirkači", names.arg=sezona2014$Driver[1:5])
+        xlab="Dirkači", names.arg=sezona2014$Driver[1:5], cex.names = 0.75)
 dev.off()
 
