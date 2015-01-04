04f5a9a2a91929f1b101cec2a1174938b662707f
Anja Zavrl <zavrl.anja@gmail.com>
2015-01-04 12:06:12 +0100

    popravek grafov

--- slike/grafi.R
+++ slike/grafi.R
@@ -1,9 +1,9 @@
-pdf("slike/graf1.pdf", paper="a4")
+pdf("slike/graf1.pdf")
 barplot(sezona2014$Points[1:5], main = "Število točk prvih pet dirkačev v sezoni 2014", 
         xlab="Dirkači", names.arg=sezona2014$Driver[1:5], cex.names = 0.75)
 dev.off()
 
-pdf("slike/graf2.pdf", paper="a4")
+pdf("slike/graf2.pdf")
 lbls <- row.names(konstruktorske.zmage)[1:10]
 pct <- round(konstruktorske.zmage$Total[1:10]/sum(konstruktorske.zmage$Total[1:10])*100)
 lbls <- paste(lbls, pct)
