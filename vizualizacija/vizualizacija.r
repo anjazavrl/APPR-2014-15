dc08c82a7e9b331516f0c6fc7a46433cf7fc3133
Anja Zavrl <zavrl.anja@gmail.com>
2015-01-09 14:39:50 +0100

    ...

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -9,25 +9,26 @@ svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthd
                           "Svet", "ne_110m_admin_0_countries.shp", mapa = "zemljevid",
                           encoding = "Windows-1250")
 
+
 t <- table(Stevilo.naslovov.dirkaca$Država)
 
 najvec.zmag <- max(t)
 najmanj.zmag <- min(t)
+
+u <- unique(t)
+u <- u[order(u)]
+barve <- rgb(0, 0, 1, match(t, u)/length(u))
 
 
 # Narišimo zemljevid v PDF.
 cat("Rišem zemljevid...\n")
 pdf("slike/Zmage_po_drzavah.pdf", width=10, height=8)
 
-n = max(t)
-u <- unique(t)
-u <- u[order(u)]
-barve <- rgb(0, 0, 1, match(t, u)/length(u))
 plot(svet, col = barve[as.character(svet$name_long)], border = "grey")
 title("Število svetovnih prvakov po državah")
 legend("left", legend = u, fill = rgb(0, 0, 1, (1:length(u))/length(u)), bg="white")
 
 #imena držav
-text(coordinates(drzave[c("dolzina", "sirina")]), labels = drzave$drzava, col = "black", cex = 0.8)
+text(coordinates(drzave[c("dolzina", "sirina")]), labels = drzave$drzava, col = "black", cex = 0.6)
 
 dev.off()
