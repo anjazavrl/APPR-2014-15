cc7ba4bee93e0d2beee1466507b8a6afc198c4e8
Anja Zavrl <zavrl.anja@gmail.com>
2015-01-09 14:02:16 +0100

    dodan zemljevid

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -5,42 +5,29 @@ source("lib/uvozi.zemljevid.r")
 
 # Uvozimo zemljevid.
 cat("Uvažam zemljevid...\n")
-obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                          "obcine", "OB/OB.shp", mapa = "zemljevid",
+svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip",
+                          "Svet", "ne_110m_admin_0_countries.shp", mapa = "zemljevid",
                           encoding = "Windows-1250")
 
-# Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
-preuredi <- function(podatki, zemljevid) {
-  nove.obcine <- c()
-  manjkajo <- ! nove.obcine %in% rownames(podatki)
-  M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
-  names(M) <- names(podatki)
-  row.names(M) <- nove.obcine[manjkajo]
-  podatki <- rbind(podatki, M)
-  
-  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
-  if (ncol(podatki) == 1) {
-    out <- data.frame(out)
-    names(out) <- names(podatki)
-    rownames(out) <- rownames(podatki)
-  }
-  return(out)
-}
+t <- table(Stevilo.naslovov.dirkaca$Država)
 
-# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, obcine)
+najvec.zmag <- max(t)
+najmanj.zmag <- min(t)
 
-# Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
 
 # Narišimo zemljevid v PDF.
 cat("Rišem zemljevid...\n")
-pdf("slike/povprecna_druzina.pdf", width=6, height=4)
+pdf("slike/Zmage_po_drzavah.pdf", width=6, height=4)
 
-n = 100
-barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
-plot(obcine, col = barve)
+n = max(t)
+u <- unique(t)
+u <- u[order(u)]
+barve <- rgb(0, 0, 1, match(t, u)/length(u))
+plot(svet, col = barve[as.character(svet$name_long)], border = "grey")
+title("Število naslovov po državah")
+legend("left", legend = u, fill = rgb(0, 0, 1, (1:length(u))/length(u)), bg="white" )
+
+#imena držav
+text(coordinates(drzave[c("dolzina", "sirina")]), labels = drzave$drzava, col = "black", cex = 0.3)
 
 dev.off()
